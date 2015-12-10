c9f995bf375d358ab78649bdc062d43bc9a484b6
Dominic <meiserdo@web.de>
2015-12-10 21:32:23 +0100

    travis setup

--- intranet.sh
+++ intranet.sh
@@ -25,13 +25,19 @@ then
 	spec="linux-g++"
 fi
 
+qtver=""
+if [ "$2" != "" ]
+then
+	qtver="-qt=$2"
+fi
+
 cwd="$PWD"
 cd "`dirname "$0"`"
 
 echo -e "\e[1m==> Building intranet with $spec ...\e[0m"
 mkdir -p build/
 cd build/
-qmake -spec "$spec" ../KFG-Intranet.pro "$debug" || exit 1
+qmake $qtver -spec "$spec" ../KFG-Intranet.pro $debug || exit 1
 make -j4 || exit 1
 
 export "PATH=$PWD:$PATH"
@@ -41,9 +47,12 @@ do
 	export "PATH=$path:$PATH"
 	export "LD_LIBRARY_PATH=$path:$LD_LIBRARY_PATH"
 done
-echo $PATH
-echo $LD_LIBRARY_PATH
+#echo $PATH
+#echo $LD_LIBRARY_PATH
 cd "$cwd"
 
-echo -e "\e[1m==> Starting intranet ...\e[0m"
-echo -e "run \"`dirname "$0"`/config.ini\"\nbt\nq\ny" | gdb -q "`dirname "$0"`/build/Intranet/intranet"
+if [ "$3" != "buildonly" ]
+then
+    echo -e "\e[1m==> Starting intranet ...\e[0m"
+    echo -e "run \"`dirname "$0"`/config.ini\"\nbt\nq\ny" | gdb -q "`dirname "$0"`/build/Intranet/intranet"
+fi
