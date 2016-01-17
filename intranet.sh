56ae036d4db90b1e472e5de66ac4ed5a443e2717
Dominic <meiserdo@web.de>
2016-01-17 16:21:18 +0100

    use different output directories

--- intranet.sh
+++ intranet.sh
@@ -1,42 +1,44 @@
 #!/bin/bash
 
 if [ "$1" == "--help" ]; then
-        echo "Usage: $0 [release]"
-        echo "       $0 --help"
-        exit 0
+    echo "Usage: $0 [release [<qt-version> [buildonly] ] ]"
+    echo "       $0 --help"
+    exit 0
 fi
 
 debug="CONFIG+=debug"
+relname="debug"
 if [ "$1" == "release" ]; then
-        debug=""
+    debug=""
+    relname="release"
 fi
 
 spec="linux-g++"
 if command -v clang &>/dev/null
 then
-	spec="linux-clang"
+    spec="linux-clang"
 fi
 if [ "$CC" == "clang" ] || [ "$CXX" == "clang++" ]
 then
-	spec="linux-clang"
+    spec="linux-clang"
 fi
 if [ "$CC" == "gcc" ] || [ "$CXX" == "g++" ]
 then
-	spec="linux-g++"
+    spec="linux-g++"
 fi
 
 qtver=""
 if [ "$2" != "" ]
 then
-	qtver="-qt=$2"
+    qtver="-qt=$2"
 fi
 
 cwd="$PWD"
 cd "`dirname "$0"`"
 
 echo -e "\e[1m==> Building intranet with $spec ...\e[0m"
-mkdir -p build/
-cd build/
+mkdir -p build-$relname/
+cd build-$relname/
 qmake $qtver -spec "$spec" ../KFG-Intranet.pro $debug || exit 1
 make -j4 || exit 1
 
@@ -44,15 +46,15 @@ export "PATH=$PWD:$PATH"
 export "LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH"
 for path in "$PWD"/*/
 do
-	export "PATH=$path:$PATH"
-	export "LD_LIBRARY_PATH=$path:$LD_LIBRARY_PATH"
+    export "PATH=$path:$PATH"
+    export "LD_LIBRARY_PATH=$path:$LD_LIBRARY_PATH"
 done
-#echo $PATH
-#echo $LD_LIBRARY_PATH
+echo $PATH
+echo $LD_LIBRARY_PATH
 cd "$cwd"
 
 if [ "$3" != "buildonly" ]
 then
     echo -e "\e[1m==> Starting intranet ...\e[0m"
-    echo -e "run \"`dirname "$0"`/config.ini\"\nbt\nq\ny" | gdb -q "`dirname "$0"`/build/Intranet/intranet"
+    echo -e "run \"`dirname "$0"`/config.ini\"\nbt\nq\ny" | gdb -q "`dirname "$0"`/build-$relname/Intranet/intranet"
 fi
