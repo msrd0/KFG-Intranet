48f2f922716eaeed77a76f2a847061d485df10ae
Dominic <meiserdo@web.de>
2016-10-08 21:42:09 +0200

    some font fixes on index page

--- Intranet/static/main.js
+++ Intranet/static/main.js
@@ -15,9 +15,13 @@ window.addEventListener("resize", apply1to1);
 
 function coloritems()
 {
-    var begin = [ 0x3A, 0xD0, 0x10 ];
-    var end   = [ 0x10, 0x20, 0xE0 ];
-    var b2e   = [ end[0]-begin[0], end[1]-begin[1], end[2]-begin[2] ];
+    var bgbegin = [ 0x3A, 0xD0, 0x10 ];
+    var bgend   = [ 0x10, 0x20, 0xE0 ];
+    var bgb2e   = [ bgend[0]-bgbegin[0], bgend[1]-bgbegin[1], bgend[2]-bgbegin[2] ];
+    
+    var fgbegin = [ 0x80, 0x40, 0x10 ];
+    var fgend   = [ 0xFF, 0x40, 0x00 ];
+    var fgb2e   = [ fgend[0]-fgbegin[0], fgend[1]-fgbegin[1], fgend[2]-fgbegin[2] ];
     
     var rows = $('.row');
     var diag = Math.sqrt(9 + (rows.length-1) * (rows.length-1));
@@ -30,11 +34,16 @@ function coloritems()
         for (var j = 0; j < cells.length; j++)
         {
             var diff = Math.sqrt(i*i + j*j) / diag;
-            var color = [ begin[0], begin[1], begin[2] ];
+            var bgcolor = [ bgbegin[0], bgbegin[1], bgbegin[2] ];
+            var fgcolor = [ fgbegin[0], fgbegin[1], fgbegin[2] ];
             for (var a = 0; a < 3; a++)
-                color[a] += diff * b2e[a];
+            {
+                bgcolor[a] += diff * bgb2e[a];
+                fgcolor[a] += diff * fgb2e[a];
+            }
             
-            $(cells[j]).children(".1to1").css("background-color", "rgb(" + (color[0]|0) + "," + (color[1]|0) + "," + (color[2]|0) + ")");
+            $(cells[j]).children(".1to1").css("background-color", "rgb(" + (bgcolor[0]|0) + "," + (bgcolor[1]|0) + "," + (bgcolor[2]|0) + ")");
+            $(cells[j]).children(".1to1").css("color"           , "rgb(" + (fgcolor[0]|0) + "," + (fgcolor[1]|0) + "," + (fgcolor[2]|0) + ")");
         }
     }
 }
